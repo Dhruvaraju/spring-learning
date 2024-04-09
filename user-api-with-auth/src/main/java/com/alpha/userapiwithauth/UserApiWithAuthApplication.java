54eb6e39d0be705dc15cee7655aaf1f62a0b7215
Dhruva Raju <49dhruva@gmail.com>
2024-04-09 23:07:20 +0530

    feat(api-with-auth): example protected apis

---
+++ user-api-with-auth/src/main/java/com/alpha/userapiwithauth/UserApiWithAuthApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.userapiwithauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiWithAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApiWithAuthApplication.class, args);
+    }
+
+}
